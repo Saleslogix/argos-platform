7d1cf19934f2b35f5cf86c007b3b318b347cf65e
Jason Best <jason.best@infor.com>
2026-02-02 10:50:30 -0500

    Fix tests.

--- products/argos-saleslogix/grunt-tasks/grunt-connect.js
+++ products/argos-saleslogix/grunt-tasks/grunt-connect.js
@@ -2,7 +2,7 @@ module.exports = function gruntConnect(grunt) {
   grunt.config('connect', {
     server: {
       options: {
-        port: 8001,
+        port: 8002,
         hostname: '127.0.0.1',
         base: '../../',
         directory: '../../',
